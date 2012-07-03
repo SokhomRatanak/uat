4bbfd6c58d35ce83d6c0595253419f6883ce2aa8
Christian Trabold <christian.trabold@dkd.de>
2012-07-03 07:07:03 +0700

    Remove steps that appear twice

--- features/steps/new_steps.rb
+++ features/steps/new_steps.rb
@@ -1,33 +1,3 @@
-Given /^PENDING/ do
-  pending
-end
-
-When /^I click on image "([^"]*)"$/ do |arg1|
-  find("##{arg1}").click
-end
-
-Wenn /^ich das Bild "([^"]*)" anklicke$/ do |arg1|
-  find("##{arg1}").click
-end
-
-Then /^I should see a date within "([^"]*)"$/ do |selector|
-  with_scope(selector) do
-      page.text.should match(/(\d{2})\.(\d{2})\.(\d{4})/)
-  end
-end
-
-Given /^I am logged in$/ do
-  page.driver.browser.credentials = "user:pw"
-end
-
-Given /^I am a developer$/ do
-  find("##{arg1}").click
-end
-
-When /^I wait until "([^"]*)" is visible$/ do |selector|
-  page.has_css?("#{selector}", :visible => true)
-end
-
 Given /^I am a backend user$/ do
   pending # express the regexp above with the code you wish you had
 end

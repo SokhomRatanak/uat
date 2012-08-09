9ad583fed6636928d60e1b1fedf938b14190e2ba
Christian Trabold <christian.trabold@dkd.de>
2012-08-09 22:25:32 +0700

    Add example how to change the driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,4 +11,9 @@ Capybara.javascript_driver = :poltergeist
 Capybara.run_server = false
 Capybara.app_host = ENV['CUCUMBER_HOST']
 
+# INFO ct 2012-08-9 Change driver to selenium. This way a firefox browser will be opened and used for the tests.
+# This also requires some changes to the steps eg. screenshot steps
+# Because capybara does not handle the screenshot API IIRC
+# Capybara.default_driver = Capybara.current_driver = :selenium
+
 World(Capybara)
